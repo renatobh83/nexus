4fc47b7933d8848a37a6fcde48d1086cd626f178
Renato <neo_rlm@hotmail.com>
2025-11-14 16:59:06 -0300

    udpatePac

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,8 @@
+import path from "node:path";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: path.join("prisma"),
   migrations: {
     path: "prisma/migrations",
   },
