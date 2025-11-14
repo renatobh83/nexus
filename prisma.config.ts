9091666438f9ce02480965f68668afb2dd0b8503
Renato <neo_rlm@hotmail.com>
2025-11-14 17:02:57 -0300

    tes

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,7 @@ import path from "node:path";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: path.join("prisma"),
+  schema: path.join("prisma", "schema.prisma"),
   migrations: {
     path: "prisma/migrations",
   },
