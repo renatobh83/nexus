fc6179264395a69c08e23c1728e759e611c0a786
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2025-11-16 15:42:06 -0300

    Select tenant name in WhatsApp repository query

--- src/core/whatsapp/whatsapp.repository.ts
+++ src/core/whatsapp/whatsapp.repository.ts
@@ -11,7 +11,7 @@ export class WhatsappRepository {
     return prisma.whatsapp.findMany({
       where: {
         isDeleted: false
-      }, include:{apiConfigs: true, tenant:true}
+      }, include:{apiConfigs: true, tenant:{select:{name:true}}}
     });
   }
   /**
