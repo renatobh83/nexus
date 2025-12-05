2c91e052d1600b56ce0afe2a9eb3fcb7b5599e60
Renato <neo_rlm@hotmail.com>
2025-12-05 14:25:45 -0300

    500

--- src/ultis/eventBus.ts
+++ src/ultis/eventBus.ts
@@ -4,6 +4,6 @@ import { EventEmitter } from "events";
 const eventBus = new EventEmitter();
 
 // aumenta limite para evitar warnings caso tenha muitas instâncias
-eventBus.setMaxListeners(200);
+eventBus.setMaxListeners(500);
 
 export { eventBus };
