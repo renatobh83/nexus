5abd283a331c35fba136d0adbbedc1f0087c6877
Renato <neo_rlm@hotmail.com>
2025-12-16 13:01:09 -0300

    return

--- src/api/helpers/Wbot/SendWhatsAppMessageList.ts
+++ src/api/helpers/Wbot/SendWhatsAppMessageList.ts
@@ -1,9 +1,10 @@
-import { Ticket } from "@prisma/client";
+import { enum_Messages_sendType, Ticket } from "@prisma/client";
 import type { Chat, Message as WbotMessage } from "wbotconnect";
 import { getWbot } from "../../../lib/wbot";
 import { AppError } from "../../../errors/errors.helper";
 import { logger } from "../../../ultis/logger";
 import VerifyMessage from "./VerifyMessage";
+import { MessageStatus } from "../../../core/messages/message.type";
 
 interface Request {
   options: any;
@@ -25,7 +26,24 @@ export const SendWhatsMessageList = async ({
     // const contact = await VerifyContact(chat, ticket.tenantId);
     await VerifyMessage(sendedMessage, ticket, ticket.contact);
 
-    return sendedMessage;
+    const messageReturn: any = {
+      id: sendedMessage.id,
+      messageId: sendedMessage.id,
+      ticketId: ticket.id,
+      contactId: ticket.contactId,
+      ack: sendedMessage.ack,
+      fromMe: sendedMessage.fromMe,
+      timestamp: sendedMessage.timestamp,
+      body: sendedMessage.body,
+      read: sendedMessage.fromMe,
+      status: sendedMessage.fromMe
+        ? ("sended" as MessageStatus)
+        : ("received" as MessageStatus),
+      tenantId: ticket.tenantId,
+      sendType: "chat" as enum_Messages_sendType,
+    };
+
+    return messageReturn;
   } catch (err: any) {
     logger.error(`SendWhatsMessageList | Error: ${err}`);
     // await StartWhatsAppSessionVerify(ticket.whatsappId, err);
