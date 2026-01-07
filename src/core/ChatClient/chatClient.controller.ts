b275f88e50aea4f010e9a7ed5b04145e7db01435
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2026-01-07 19:04:17 -0300

    Remove chat_token cookie from response
    Removed setting cookie for chat_token in response.

--- src/core/ChatClient/chatClient.controller.ts
+++ src/core/ChatClient/chatClient.controller.ts
@@ -77,11 +77,6 @@ export async function chatClientController(
       const token = sign(payload, JWT_SECRET, {
         expiresIn: "360m", // tempo de vida do token
       });
-      reply.setCookie("chat_token", token, {
-        httpOnly: true,
-        sameSite: "lax",
-        maxAge: 24 * 60 * 60 * 1000,
-      });
 
       try {
         return reply.code(200).send({ token });
