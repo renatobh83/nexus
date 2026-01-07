818f45981dca8086c599f562296ffd005d0d9bd9
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2026-01-07 18:55:01 -0300

    Fix conditional logic for ticket creation

--- src/api/helpers/CreateTicketSafe.ts
+++ src/api/helpers/CreateTicketSafe.ts
@@ -81,7 +81,7 @@ export const findOrCreateTicketSafe = async (params: {
 console.log(msg)
       if (
         (msg && !msg.fromMe) ||
-        (!newTicket.userId  && chatClient && !msg.fromMe)
+        (!newTicket.userId  && !chatClient && !msg.fromMe)
       ) {
         const ticket = await ChatFlow.CheckChatBotFlowWelcome(newTicket);
         newTicket = ticket ? ticket : newTicket;
