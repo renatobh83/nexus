d76d3913d1edef8295b48547dc55446d4a4ec003
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2026-01-07 17:52:18 -0300

    Log socket handshake information

--- src/api/index.ts
+++ src/api/index.ts
@@ -152,7 +152,7 @@ async function buildServer(): Promise<FastifyInstance> {
       try {
         const req = { headers: { cookie: socket.handshake.headers.cookie } };
         const cookies = req.headers.cookie;
-console.log(req)
+console.log(socket.handshake)
         const accessToken = cookies
           ?.split("; ")
           .find((c) => c.startsWith("chat_token="))
