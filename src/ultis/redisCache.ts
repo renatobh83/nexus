8f0237797e24d938988b8049492d419ec2dd506c
Renato <neo_rlm@hotmail.com>
2026-01-10 14:03:15 -0300

    Ttls

--- src/ultis/redisCache.ts
+++ src/ultis/redisCache.ts
@@ -9,7 +9,7 @@ export async function getCache<T>(key: string): Promise<T | null> {
 }
 
 // Salvar no cache com TTL
-export async function setCache<T>(key: string, value: T, ttlSeconds = 120) {
+export async function setCache<T>(key: string, value: T, ttlSeconds = 300) {
   await redisClient.set(key, JSON.stringify(value), "EX", ttlSeconds);
 }
 
